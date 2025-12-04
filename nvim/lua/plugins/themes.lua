b4ba56e7498d2fbe751bebc1ddb0177235b0b030
Robert Blafford <robert.blafford@datadoghq.com>
2025-12-04 10:12:54 -0500

    Modify themes

--- nvim/lua/plugins/themes.lua
+++ nvim/lua/plugins/themes.lua
@@ -7,8 +7,8 @@ return {
     "sainnhe/sonokai",
     config = function()
       -- Set Sonokai options
-      -- vim.g.sonokai_style = "default" -- other options: default, atlantis, shusia, maia, espresso
-      -- vim.g.sonokai_transparent_background = 1
+      vim.g.sonokai_style = "default" -- other options: default, atlantis, shusia, maia, espresso
+      vim.g.sonokai_transparent_background = 1
     end,
   },
   { "vague2k/vague.nvim" },
@@ -19,7 +19,7 @@ return {
     "EdenEast/nightfox.nvim",
     opts = {
       options = {
-        transparent = true,
+        transparent = false,
       },
     },
   },
@@ -27,7 +27,7 @@ return {
     "LazyVim/LazyVim",
     opts = {
       -- Set the enabled default theme here
-      colorscheme = "nightfox",
+      colorscheme = "tokyonight-night",
     },
   },
 }
